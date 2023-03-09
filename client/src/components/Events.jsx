6bb8a25dbba3c3a135d99ad96bf142799bc6ecbc
Quentin Moreaux <quentin@cblue.fr>
2023-03-09 15:43:33 +0100

    Remove comment in Events.jsx

--- client/src/components/Events.jsx
+++ client/src/components/Events.jsx
@@ -1,30 +1,30 @@
-import { Typography } from "@mui/material";
-import React, { useEffect, useState, useCallback } from "react";
-import useEth from "../contexts/EthContext/useEth";
+import { Typography } from '@mui/material';
+import React, { useEffect, useState, useCallback } from 'react';
+import useEth from '../contexts/EthContext/useEth';
 
 export default function Events() {
   const [events, setEvents] = useState([]);
   const {
-    state: { contract },
+    state: { contract }
   } = useEth();
 
   const getOldEvents = useCallback(async () => {
-    let oldEvents = await contract.getPastEvents("VoterRegistered", {
+    let oldEvents = await contract.getPastEvents('VoterRegistered', {
       fromBlock: 0,
-      toBlock: "latest",
+      toBlock: 'latest'
     });
     setEvents(oldEvents);
   }, [contract]);
 
   const getEvents = useCallback(async () => {
-    // await contract.events
-    //   .valueChanged({ fromBlock: "earliest" })
-    //   .on("data", (event) => {
-    //     setEvents([...events, event]);
-    //   })
-    //   .on("changed", (changed) => console.log(changed))
-    //   .on("error", (err) => console.log(err))
-    //   .on("connected", (str) => console.log(str));
+    await contract.events
+      .valueChanged({ fromBlock: 'earliest' })
+      .on('data', (event) => {
+        setEvents([...events, event]);
+      })
+      .on('changed', (changed) => console.log(changed))
+      .on('error', (err) => console.log(err))
+      .on('connected', (str) => console.log(str));
   }, [contract, events]);
 
   useEffect(() => {
@@ -39,9 +39,7 @@ export default function Events() {
       <Typography variant="h6"> Voters:</Typography>
 
       {events.map((event) => (
-        <Typography key={event.id}>
-          {event.returnValues.voterAddress}
-        </Typography>
+        <Typography key={event.id}>{event.returnValues.voterAddress}</Typography>
       ))}
     </div>
   );
