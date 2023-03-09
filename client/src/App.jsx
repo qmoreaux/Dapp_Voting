ed3380e62ce06d507e24910e3085097183e4eb0f
samReact <sam.react001@gmail.com>
2023-03-09 13:25:46 +0100

    :construction: main, admin component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { EthProvider } from "./contexts/EthContext";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <EthProvider>
       <Header />
+      <Main />
       <Footer />
     </EthProvider>
   );
---
+++ client/src/components/Admin.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export default function Admin() {
+  return (
+    <>
+      <div>Hi</div>
+    </>
+  );
+}
