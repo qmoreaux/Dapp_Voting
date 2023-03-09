2682dc659611c917fc90dabc30851957319521ef
samReact <sam.react001@gmail.com>
2023-03-09 14:08:28 +0100

    :construction: admin - add to whitelist

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -1,9 +1,47 @@
-import * as React from "react";
+import { Button, TextField } from "@mui/material";
+import React, { useState } from "react";
+
+import useEth from "../contexts/EthContext/useEth";
 
 export default function Admin() {
+  const [address, setAddress] = useState("");
+  const [error, setError] = useState(false);
+
+  const {
+    state: { contract, accounts, web3 },
+  } = useEth();
+
+  async function addVoter() {
+    if (web3.utils.isAddress(address)) {
+      try {
+        await contract.methods.addVoter(address).send({ from: accounts[0] });
+        console.log("success");
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      setError(true);
+    }
+  }
   return (
     <>
-      <div>Hi</div>
+      <div style={{ margin: 100 }}>
+        <TextField
+          size="small"
+          error={error}
+          id="outlined-error-helper-text"
+          label="Voter Address"
+          defaultValue=""
+          helperText={error && "Invalid address"}
+          onChange={(event) => {
+            setError(false);
+            setAddress(event.target.value);
+          }}
+        />
+        <Button variant="contained" onClick={addVoter}>
+          Add
+        </Button>
+      </div>
     </>
   );
 }
