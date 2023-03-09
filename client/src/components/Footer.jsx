f58e7c3cb8c444ff724571b60be34fa1740b9bf2
samReact <sam.react001@gmail.com>
2023-03-09 13:05:44 +0100

    add simple text for footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,12 +2,15 @@ import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
+import { Typography } from "@mui/material";
 
 export default function Footer() {
   return (
     <AppBar position="fixed" color="primary" sx={{ top: "auto", bottom: 0 }}>
       <Toolbar>
-        <Box sx={{ flexGrow: 1 }} />
+        <Box sx={{ flexGrow: 1, justifyContent: "center", display: "flex" }}>
+          <Typography>Made by Quentin and Samir for Alyra</Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   );
