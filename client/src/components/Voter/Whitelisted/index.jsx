6fcc0e3ea5448ca046d3a9776fbc2ad4d52f43fd
Quentin Moreaux <quentin@cblue.fr>
2023-03-09 16:09:59 +0100

    Implements component for SubmitVote and SubmitProposal

--- client/src/components/Voter/Whitelisted/index.jsx
+++ client/src/components/Voter/Whitelisted/index.jsx
@@ -1,3 +1,8 @@
+import { useEffect, useState, useCallback } from 'react';
+
+import SubmitProposal from './SubmitProposal';
+import SubmitVote from './SubmitVote';
+
 import useEth from '../../../contexts/EthContext/useEth';
 
 export default function Voter() {
@@ -5,11 +10,24 @@ export default function Voter() {
     state: { contract, accounts, web3 }
   } = useEth();
 
- 
+  const [workflowStatus, setWorkflowStatus] = useState(0);
+
+  const getCurrentWorkflowStatus = useCallback(async () => {
+    const currentWorkflowStatus = await contract.methods.workflowStatus().call();
+    setWorkflowStatus(currentWorkflowStatus);
+  }, [contract]);
+
+  useEffect(() => {
+    if (contract) {
+      getCurrentWorkflowStatus();
+    }
+  }, [contract, getCurrentWorkflowStatus]);
+
   return (
     <>
       <div>
         You are whitelisted. Congrats !
+        {workflowStatus === 1 ? <SubmitProposal /> : workflowStatus === 3 ? <SubmitVote /> : 'Nothing to do for now'}
       </div>
     </>
   );
