5b5801afff84831ea7e71fa1e0f4264a87b4d755
samReact <sam.react001@gmail.com>
2023-03-10 12:07:28 +0100

    :construction: stepper update

--- client/src/components/Main/Stepper.jsx
+++ client/src/components/Main/Stepper.jsx
@@ -1,17 +1,20 @@
-import * as React from "react";
-import { Box, Stepper, Step, StepLabel } from "@mui/material";
+import { Box, Stepper, Step, StepLabel } from '@mui/material';
+
+import useStatus from '../../hooks/useStatus';
 
 const steps = [
-  "Voters Registration",
-  "Proposals Registration",
-  "Voting Session",
-  "Votes Tallied",
+  'Voters Registration',
+  'Proposals Registration',
+  'Voting Session',
+  'Votes Tallied'
 ];
 
 export default function HorizontalStepper() {
+  const status = useStatus();
+
   return (
-    <Box sx={{ width: "100%", mt: 6 }}>
-      <Stepper activeStep={0} alternativeLabel>
+    <Box sx={{ width: '100%', mt: 6 }}>
+      <Stepper activeStep={status} alternativeLabel>
         {steps.map((label) => (
           <Step key={label}>
             <StepLabel>{label}</StepLabel>
