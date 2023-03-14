8df834a9c4ef0dba64eb9bc0fe16e517e8533b26
Quentin Moreaux <quentin@cblue.fr>
2023-03-14 12:56:20 +0100

    Add owner to whitelist on deploy

--- truffle/migrations/1_deploy_voting.js
+++ truffle/migrations/1_deploy_voting.js
@@ -1,5 +1,7 @@
 const Voting = artifacts.require('Voting');
 
-module.exports = function (deployer) {
-    deployer.deploy(Voting);
+module.exports = async (deployer, network, accounts) => {
+    await deployer.deploy(Voting, { from: accounts[0] });
+    const votingInstance = await Voting.deployed();
+    await votingInstance.addVoter(accounts[0]);
 };
