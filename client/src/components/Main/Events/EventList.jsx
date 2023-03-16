2a59537ae7aae1191c7e0f92ced0107f358d0431
Quentin Moreaux <quentin@cblue.fr>
2023-03-16 11:50:11 +0100

    Fix key issue in EventList

--- client/src/components/Main/Events/EventList.jsx
+++ client/src/components/Main/Events/EventList.jsx
@@ -7,31 +7,33 @@ export default function EventList({ title, type, events }) {
 
   return (
     <Stack>
-      <Typography variant="h6">{title}</Typography>
+      <Typography key="title" variant="h6">
+        {title}
+      </Typography>
       {events.map((event) => (
-        <>
+        <Typography key={event.id}>
           {type === 'voters' ? (
-            <Typography key={event.id}>
+            <>
               {event.returnValues.new ? 'New voter' : 'Voter'} registered :{' '}
               <b>{getShortAddress(event.returnValues.voterAddress)}</b>
-            </Typography>
+            </>
           ) : (
             <>
               {type === 'proposals' ? (
-                <Typography key={event.id}>
+                <>
                   {event.returnValues.new ? 'New proposal' : 'Proposal'} submitted{' '}
                   <b>{event.returnValues.proposalId}</b> : {event.returnValues.description}
-                </Typography>
+                </>
               ) : (
-                <Typography key={event.id}>
+                <>
                   {event.returnValues.new ? 'New vote' : 'Vote'} submitted :{' '}
                   <b>{getShortAddress(event.returnValues.voter)}</b> voted for proposal{' '}
                   <b>{event.returnValues.proposalId}</b>
-                </Typography>
+                </>
               )}
             </>
           )}
-        </>
+        </Typography>
       ))}
     </Stack>
   );
